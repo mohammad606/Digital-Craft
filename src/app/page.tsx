3b80be56a2bd8af126419fd366ef2a2bd0199310
محمد halabi <mohammad123alhalabi123@gmail.com>
2025-04-05 01:42:17 +0200

    create contact us and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import OurSection from "@/components/sections/our";
 import ProcessSection from "@/components/sections/process";
 import WorkSection from "@/components/sections/work";
 import WhyAus from "@/components/sections/whyAus";
+import ContactSection from "@/components/sections/contact";
+import FooterSection from "@/components/sections/footer";
 
 export default function Home() {
     return (
@@ -16,6 +18,8 @@ export default function Home() {
             <ProcessSection/>
             <WorkSection/>
             <WhyAus/>
+            <ContactSection/>
+            <FooterSection/>
         </>
     );
 }
